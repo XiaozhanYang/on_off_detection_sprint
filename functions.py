e5a26d7462f64466d6bd97cb641545a0f1c1cebc
Cody Yang <xiaozhan.yang@icloud.com>
2022-05-31 15:47:43 +0100

    modify the query_metadata function, considering meta_config and default_config as inputs.

--- functions.py
+++ functions.py
@@ -80,25 +80,66 @@ def query_iot_data(db, query_start_time, query_end_time, nid, channels_list=None
                        'nid': pd.Series(dtype='str'),
                           'channel':pd.Series(dtype='str')})
         return df
- 
-def query_metadata(db, concerned_assets_from_sites = None, 
-                   columns_for_join = ['nid', 'channel'], 
-                   columns_for_pivot = ['site_name', 'asset_type']):
-    
-    assets_list = []
-    for assets_from_selected_site in concerned_assets_from_sites:
-        assets_from_selected_site["site_name"]
-        for asset_type in assets_from_selected_site["asset_type"]:
-            assets_list.append(f"""('{assets_from_selected_site["site_name"]}', '{asset_type}')""")
-            
-    columns_for_query_str = ', '.join(db.columns_for_query)
 
-    columns_for_pivot_str = ', '.join(columns_for_pivot)
+def extract_columns(meta_config):
+    columns_for_query = set()
+    for meta_config_record in meta_config:
+        for select_slice in meta_config_record['select']:
+            columns_for_query = columns_for_query.union(set(select_slice))
+        for exclude_slice in meta_config_record['exclude']:
+            columns_for_query = columns_for_query.union(set(exclude_slice))
             
-    concerned_assets_str = ", ".join(assets_list)
+    return list(columns_for_query)
+
+def meta_select_config(df_meta, meta_config, default_config):
+    
+    index_whole = False
+
+    for meta_config_record in meta_config:
+        # can be a generic function in the next step
+        index_select = False
+        for select_slice in meta_config_record['select']:
+            index_select_slice = True
+            for column in select_slice:
+                index_select_slice &= (df_meta[column].isin(select_slice[column]))
+            index_select |= index_select_slice   
+
+        index_exclude = False
+        for exclude_slice in meta_config_record['exclude']:
+            index_exclude_slice = True
+            for column in exclude_slice:
+                index_exclude_slice &= (df_meta[column].isin(exclude_slice[column]))
+            index_exclude |= index_exclude_slice
+
+        index_record = index_select & (~index_exclude)
+
+        for param in meta_config_record['config']:
+            # also add the config from default
+            df_meta.loc[index_record, param] = meta_config_record['config'][param]
+
+        index_whole |= index_record
+
+    df_meta_select_config = df_meta.loc[index_whole]
+    
+    return df_meta_select_config
+
+def query_metadata(db, meta_config, default_config,
+                   columns_for_pivot = ['site_name', 'circuit_description'],
+                   columns_for_join = ['nid', 'channel']):
+    
+    # assets_list = []
+    # for assets_from_selected_site in concerned_assets_from_sites:
+    #     assets_from_selected_site["site_name"]
+    #     for asset_type in assets_from_selected_site["asset_type"]:
+    #         assets_list.append(f"""('{assets_from_selected_site["site_name"]}', '{asset_type}')""")
+            
+    columns_for_config = extract_columns(meta_config)
+
+    columns_for_query = set(columns_for_config).union(set(columns_for_pivot),set(columns_for_join))
+    columns_for_query_str = ', '.join(columns_for_query)
         
     # "Dishwasher", "Pizza Oven", "Intruder Alarm"
-    query  = f"""SELECT {columns_for_query_str} from {db.table_name} where ({columns_for_pivot_str}) = ANY (VALUES {concerned_assets_str})"""
+    query  = f"""SELECT {columns_for_query_str} from {db.table_name};"""
 
     conn = psycopg2.connect(db.CONNECTION)
 
@@ -111,9 +152,12 @@ def query_metadata(db, concerned_assets_from_sites = None,
 
     conn.close()
     
-    df_meta['channel'] = df_meta.channel_number.astype(str)
+    for column in columns_for_join:
+        df_meta[column] = df_meta[column].astype(str)
     
-    return df_meta[[*columns_for_join, *columns_for_pivot]]
+    df_meta_select_config = meta_select_config(df_meta, meta_config, default_config)
+
+    return df_meta_select_config[[*columns_for_join, *columns_for_pivot]]
 
 def db_read_query(db, query_start_time, query_end_time, df_meta, 
                   columns_for_join = ['nid', 'channel']):
