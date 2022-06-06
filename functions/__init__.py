60523fad1622106aeb8b290cf41535f4ee9fd776
Shashank Goud <s.goud@cloudfmgroup.com>
2022-06-06 11:24:28 +0100

    Added folder functions in the package structure

--- functions/__init__.py
+++ functions/__init__.py
@@ -4,4 +4,3 @@ from .batch_processing import batch_processing
 # For testing purposes:
 from .db_read_query import db_read_query
 from .scan_on_off_from_queried_data import scan_on_off_from_queried_data
-
