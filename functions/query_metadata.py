26e8ad407761a88ac4eb0fcf47909426132652c7
Cody Yang <xiaozhan.yang@icloud.com>
2022-06-06 16:38:49 +0100

    remove debugging lines in query_metadata

--- functions/query_metadata.py
+++ functions/query_metadata.py
@@ -83,5 +83,4 @@ def query_metadata(db, meta_config, default_config,
     
     df_meta_select_config = meta_select_config(df_meta, meta_config, default_config)
 
-    print('line before return - for testing.')
     return df_meta_select_config[[*columns_for_join, *columns_for_pivot, *default_config]]
