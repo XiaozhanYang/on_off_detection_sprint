1ab78ca778e2d8dba438e859c307492c2b4039e6
Cody Yang <xiaozhan.yang@icloud.com>
2022-06-06 16:37:33 +0100

    remove debugging lines in query_metadata

--- functions/query_metadata.py
+++ functions/query_metadata.py
@@ -37,13 +37,10 @@ def meta_select_config(df_meta, meta_config, default_config):
         for param in default_config:
             if param in meta_config_record['config']:
             # also add the config from default
-                print("param in meta_config_record['config']")
                 df_meta.loc[index_record, param] = meta_config_record['config'][param]
             elif param in df_meta.columns:
-                print('param in df_meta.columns')
                 df_meta.loc[index_record, param] = df_meta.loc[index_record, param].fillna(default_config[param])
             else:
-                print('param not in df_meta.columns')
                 df_meta.loc[index_record, param] = default_config[param]
 
         index_whole |= index_record
