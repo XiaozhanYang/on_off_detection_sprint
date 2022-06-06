18a80cc33e008118b203d66a0b6e8acdfce2aa69
Shashank Goud <s.goud@cloudfmgroup.com>
2022-06-06 11:20:32 +0100

    Added testing block of code in main calling file

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 from .query_metadata import query_metadata
 from .batch_processing import batch_processing
 
+# For testing purposes:
+from .db_read_query import db_read_query
+from .scan_on_off_from_queried_data import scan_on_off_from_queried_data
 
