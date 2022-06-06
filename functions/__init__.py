3ef293345514b8b1af5485c477821295eb50e020
Cody Yang <xiaozhan.yang@icloud.com>
2022-06-06 16:43:28 +0100

    add save_on_off_to_db to the top level functions for testing

--- functions/__init__.py
+++ functions/__init__.py
@@ -4,3 +4,4 @@ from .batch_processing import batch_processing
 # For testing purposes:
 from .db_read_query import db_read_query
 from .scan_on_off_from_queried_data import scan_on_off_from_queried_data
+from .batch_processing import save_on_off_to_db
