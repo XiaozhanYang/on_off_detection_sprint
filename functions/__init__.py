7acbc94cbcd789eeeb0ec5e1497ca4638e50c7e0
Cody Yang <xiaozhan.yang@icloud.com>
2022-06-08 16:01:51 +0100

    change save_on_off_to_db import for testing

--- functions/__init__.py
+++ functions/__init__.py
@@ -2,6 +2,6 @@ from .query_metadata import query_metadata
 from .batch_processing import batch_processing
 
 # For testing purposes:
-from .db_read_query import db_read_query
-from .scan_on_off_from_queried_data import scan_on_off_from_queried_data
-from .batch_processing import save_on_off_to_db
+from .batch_processing.db_read_query import db_read_query
+from .batch_processing.scan_on_off_from_queried_data import scan_on_off_from_queried_data
+from .batch_processing.save_on_off_to_db import save_on_off_to_db
