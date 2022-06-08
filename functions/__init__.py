c8a0a4781f30cbcf07e0e11c456a86edfb98ec81
Shashank Goud <shashaankgoud@gmail.com>
2022-06-08 12:20:27 +0100

    Testing codeQL
    Checking codeql analysis

--- functions/__init__.py
+++ functions/__init__.py
@@ -5,3 +5,8 @@ from .batch_processing import batch_processing
 from .db_read_query import db_read_query
 from .scan_on_off_from_queried_data import scan_on_off_from_queried_data
 from .batch_processing import save_on_off_to_db
+
+#Codeql Testing
+- uses: github/codeql-action/init@v2
+  with:
+    config-file: ./.github/codeql/codeql-config.yml
