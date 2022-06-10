8f0c4f53d390e5b86b09d42523fa6929d60b66f6
Cody Yang <xiaozhan.yang@icloud.com>
2022-06-10 16:56:35 +0100

    export run function from notebook to the package

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,75 +1,9 @@
 from .query_metadata import query_metadata
 from .batch_processing import batch_processing
+from .run import run
 
 # For testing purposes:
 from .batch_processing.db_read_query import db_read_query
 from .batch_processing.scan_on_off_from_queried_data import scan_on_off_from_queried_data
 from .batch_processing.save_on_off_to_db import save_on_off_to_db
 
-from influxdb import InfluxDBClient
-import psycopg2
-import pandas as pd
-
-def empty_the_existing_data_in_db(influxdb, postgresdb):
-
-    # empty the data in influxdb
-    
-    client_gen3 = InfluxDBClient(host=influxdb.host, port=influxdb.port, database=influxdb.database)
-
-    q = f""" DROP MEASUREMENT "{influxdb.sink_table}" """
-
-    client_gen3.query(q)
-
-    # empty the data in postgresdb
-  
-    conn = psycopg2.connect(postgresdb.CONNECTION)
-    cur = conn.cursor()
-
-    # check whether the table exists
-
-    query_check_existing_tables  = """SELECT table_name 
-                                      FROM information_schema.tables
-                                      WHERE table_schema = 'public'
-                                   """
-
-    cur.execute(query_check_existing_tables)
-    list_tables = cur.fetchall()
-    table_name_list  = [table_name[0] for table_name in list_tables]
-
-    if postgresdb.table_name_sink in table_name_list:
-
-        # delete existing data
-        query_empty_hypertable = f"DELETE FROM {postgresdb.table_name_sink};"
-        cur.execute(query_empty_hypertable)
-        
-    else:
-        
-        # turn on the timescaledb extension
-        list_the_extensions = "CREATE EXTENSION IF NOT EXISTS timescaledb;" # enable the timescaledb extension, so function create_hypertable can be used
-        cur.execute(list_the_extensions)
-        conn.commit()
-
-        # create timescaledb hypertable
-        query_create_normaltable = f"""CREATE TABLE IF NOT EXISTS {postgresdb.table_name_sink} (
-                                               time TIMESTAMPTZ NOT NULL,
-                                               action INTEGER,
-                                               site_name TEXT NULL,
-                                               circuit_description TEXT NULL
-                                               );"""
-
-        query_create_hypertable = f"SELECT create_hypertable('{postgresdb.table_name_sink}', 'time');"
-
-        cur.execute(query_create_normaltable) 
-        cur.execute(query_create_hypertable)
-
-    conn.commit()
-    cur.close()
-
-def calculate_padding_query_detect_from_expiration_time(df_meta, resample_padding_size, resample_freq):
-
-    expiration_time_margins = [int(df_meta.expiration_time_low.max()), int(df_meta.expiration_time_high.max())]
-    scan_padding_size = sum(expiration_time_margins) + 2 # expiration_time_margins are for the rolling window function, 1 is for diff function
-
-    padding_query_detect = (scan_padding_size + resample_padding_size) * pd.Timedelta(resample_freq)
-    
-    return padding_query_detect
--- functions/run.py
+++ functions/run.py
@@ -1,1 +1,64 @@
+import pandas as pd
+import logging
+import time
 
+from .empty_the_existing_data_in_db import empty_the_existing_data_in_db
+from .query_metadata import query_metadata
+from .calculate_padding_query_detect import calculate_padding_query_detect
+
+def run(cf, 
+        log_file_name= "logfile.log", 
+        log_level=logging.INFO, 
+        batch_size_max = pd.Timedelta("3H"),
+        batch_size_min = pd.Timedelta("5T"),
+        delay_size = pd.Timedelta("1T"),
+        time_second_for_sleep = 60):
+
+    # initialisation
+    
+    logging.basicConfig(format='%(message)s', filename=log_file_name, level=log_level)
+
+    empty_the_existing_data_in_db(cf.influxdb, cf.postgresdb)
+
+    df_meta = query_metadata(cf.postgresdb, cf.meta_config, cf.default_config,
+                               columns_for_join=cf.meta_columns_for_join, 
+                               columns_for_pivot=cf.columns_for_pivot)
+
+    padding_query_detect = calculate_padding_query_detect(df_meta, 
+                                                          cf.resample_padding_size, 
+                                                          cf.resample_freq)
+
+    start_time = pd.Timestamp(cf.start_time_str, tz="UTC")
+    batch_size = batch_size_max
+
+    start_row_value = None
+    df_buffered_rows_for_next_query = None
+
+    # loop 
+
+    while True:
+
+        if pd.Timestamp.now(tz='UTC') - (start_time + padding_query_detect) > (batch_size+delay_size):
+
+            time_range = (start_time, start_time + batch_size)
+
+            start_row_value, \
+            df_buffered_rows_for_next_query \
+            = f.batch_processing(cf.influxdb, 
+                                 cf.postgresdb, 
+                                 df_meta, 
+                                 time_range, 
+                                 cf.resample_freq, 
+                                 padding_query_detect,
+                                 columns_for_pivot=cf.columns_for_pivot, 
+                                 column_for_detect=cf.column_for_detect, 
+                                 iot_columns_for_join=cf.iot_columns_for_join, 
+                                 meta_columns_for_join=cf.meta_columns_for_join, 
+                                 start_row_value=start_row_value, 
+                                 df_buffered_rows_for_next_query=df_buffered_rows_for_next_query)
+
+            start_time = start_time + batch_size
+
+        else:
+            batch_size = batch_size_min
+            time.sleep(time_second_for_sleep)
